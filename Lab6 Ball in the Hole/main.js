ff6633be6ae0b1ebbbb138f2252dfa0555a790e4
juleenek <leeenek0@gmail.com>
2022-12-02 19:48:11 +0100

    Idea with point

--- Lab6 Ball in the Hole/main.js
+++ Lab6 Ball in the Hole/main.js
@@ -1,20 +1,24 @@
 'use strict';
 
-const SENSORS_LIMIT = 25;
+export const SENSORS_LIMIT = 25;
 const MAX_POSITION_X = 328;
 const MAX_POSITION_Y = 580;
-const SPEED_BALL = 10;
+const SPEED_BALL = SENSORS_LIMIT / 4;
 
 const ball = document.querySelector('.ball');
 const boardBox = document.querySelector('.board-box');
 
 let x, y;
-let ball_X = 0;
-let ball_Y = 0;
 let moveX = 0;
 let moveY = 0;
 let ballPositionX = 165;
-let ballPositionY= 450;
+let ballPositionY = 450;
+
+const currentGame = {
+  positivePoints: 5,
+  negativePoints: 3,
+  points: 0,
+};
 
 function handleOrientation(event) {
   x = event.gamma;
@@ -31,20 +35,61 @@ function handleOrientation(event) {
 
 window.addEventListener('deviceorientation', handleOrientation);
 
-function animate() {
-  if (
-    ballPositionX + moveX < MAX_POSITION_X &&
-    ballPositionX + moveX > 0 &&
-    ballPositionY + moveY < MAX_POSITION_Y &&
-    ballPositionY + moveY > 0
-  ) {
+function changePosition() {
+  if (ballPositionX + moveX < MAX_POSITION_X && ballPositionX + moveX > 0) {
     ballPositionX += moveX;
     ball.style.left = `${ballPositionX}px`;
+  }
+  if (ballPositionY + moveY < MAX_POSITION_Y && ballPositionY + moveY > 0) {
     ballPositionY += moveY;
     ball.style.top = `${ballPositionY}px`;
   }
-  console.log(ballPositionX);
+}
+
+function animate() {
+  changePosition();
   window.requestAnimationFrame(animate);
 }
 
 window.requestAnimationFrame(animate);
+
+function getRandomX() {
+  const random = Math.random() * MAX_POSITION_X;
+  return (random > 140 && random < 200) ||
+    random < 20 ||
+    random > MAX_POSITION_X - 20
+    ? getRandomX()
+    : random;
+}
+
+function getRandomY() {
+  const random = Math.random() * MAX_POSITION_Y;
+  return (random > 420 && random < 470) ||
+    (random > 20 && random < 70) ||
+    random < 20 ||
+    random > MAX_POSITION_Y - 20
+    ? getRandomX()
+    : random;
+}
+
+function generatePositivePoint() {
+  const point = document.createElement('div');
+  point.classList.add('positive-point');
+  point.style.left = `${getRandomX()}px`;
+  point.style.top = `${getRandomY()}px`;
+  boardBox.appendChild(point);
+}
+function generateNegativePoint() {
+  const point = document.createElement('div');
+  point.classList.add('negative-point');
+  point.style.left = `${getRandomX()}px`;
+  point.style.top = `${getRandomY()}px`;
+  boardBox.appendChild(point);
+}
+
+for (let index = 0; index < currentGame.negativePoints; index++) {
+  generateNegativePoint();
+}
+for (let index = 0; index < currentGame.positivePoints; index++) {
+  generatePositivePoint();
+}
---
+++ Lab6 Ball in the Hole/modules/difficulty.js
@@ -0,0 +1,13 @@
+import { SENSORS_LIMIT } from "../main";
+
+const easyDifficulty = {
+  speed: SENSORS_LIMIT / 2
+};
+
+const mediumDifficulty = {
+  speed: SENSORS_LIMIT / 3
+};
+
+const hardDifficulty = {
+  speed: SENSORS_LIMIT / 4
+};
